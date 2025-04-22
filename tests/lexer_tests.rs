9511a3e8109d203d7380dce2a22962045f13607d
sara alghafli <sara.alghafli@hotmail.com>
2025-04-22 19:37:25 +0400

    Create lexer_tests.rs

---
+++ tests/lexer_tests.rs
@@ -0,0 +1,31 @@
+use c4::{Lexer, Token, Position};
+
+#[test]
+fn test_lex_number() {
+    let mut lexer = Lexer::new("123");
+    assert_eq!(lexer.next_token(), Token::Number(123, Position { line: 1, column: 0 }));
+}
+
+#[test]
+fn test_lex_identifier() {
+    let mut lexer = Lexer::new("abc");
+    assert_eq!(lexer.next_token(), Token::Identifier("abc".to_string(), Position { line: 1, column: 0 }));
+}
+
+#[test]
+fn test_lex_keyword() {
+    let mut lexer = Lexer::new("return");
+    assert_eq!(lexer.next_token(), Token::Keyword("return".to_string(), Position { line: 1, column: 0 }));
+}
+
+#[test]
+fn test_lex_symbol() {
+    let mut lexer = Lexer::new("+");
+    assert_eq!(lexer.next_token(), Token::Symbol('+', Position { line: 1, column: 0 }));
+}
+
+#[test]
+fn test_eof() {
+    let mut lexer = Lexer::new("");
+    assert_eq!(lexer.next_token(), Token::Eof);
+}
