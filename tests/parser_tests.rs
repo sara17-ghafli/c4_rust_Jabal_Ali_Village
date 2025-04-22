f200347098187a1a33566a3e3903e59f53fc9661
sara alghafli <sara.alghafli@hotmail.com>
2025-04-22 20:27:44 +0400

    Update parser_tests.rs

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -1,9 +1,12 @@
 use c4::{Lexer, Parser, Token, ASTNode, VM};
 
+//tests if 'return 42;' is parsed and executed correctly
 #[test]
 fn test_parse_return() {
     let mut lexer = Lexer::new("return 42;");
     let mut tokens = Vec::new();
+
+    //collect tokens til eof
     loop {
         let t = lexer.next_token();
         if let Token::Eof = t {
@@ -14,5 +17,5 @@ fn test_parse_return() {
 
     let mut parser = Parser::new(tokens);
     let ast = parser.parse_return();
-    VM::run(ast); // Should print: Returned: 42
+    VM::run(ast); //outputs: Returned: 42
 }
