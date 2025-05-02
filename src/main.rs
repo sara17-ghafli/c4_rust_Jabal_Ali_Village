bfb790dfb7c90388c6b8a04800f2d38c189c21c3
sara alghafli <sara.alghafli@hotmail.com>
2025-05-02 16:14:38 +0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::io::{self, Read};
 use c4_rust_team_jabal_ali_village::{Lexer, Parser, Token, VM};
 
 fn main() {
+    // Read all input from stdin
     let mut source = String::new();
     io::stdin().read_to_string(&mut source).unwrap();
 
+    // Lexing: convert input into tokens
     let mut lexer = Lexer::new(&source);
     let mut tokens = vec![];
     loop {
@@ -13,8 +15,10 @@ fn main() {
         tokens.push(t);
     }
 
+    // Parsing: convert tokens into an AST
     let mut parser = Parser::new(tokens);
     match parser.parse_return() {
+        // Evaluation: run the AST
         Ok(ast) => VM::run(ast),
         Err(e) => eprintln!("Syntax Error: {}", e),
     }
